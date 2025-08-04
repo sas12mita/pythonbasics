50e861610fab11074f4fea6dcb63323b3ee226d6
sas12mita <sumitapoudel12@gmail.com>
2025-08-04 10:58:57 +0545

    day 1 learning variable

---
+++ day1-variable.py
@@ -0,0 +1,16 @@
+name="sasmita"
+print("your name is ",name)
+age=12
+print('ypur age is',age)
+is_student=False
+print('are you student',is_student)
+skill=['laravel','python','node']
+print('your skill is',skill)
+experiance={   
+    'laravel':"tukisoft",
+    'node':"self",
+    'python':"self",
+}
+print('experiance laravel in',experiance['laravel'])
+print('experiance node in',experiance['node'])
+print('experiance python in',experiance['python'])
