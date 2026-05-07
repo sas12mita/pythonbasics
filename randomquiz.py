b70b430aeecb12cf014634b2b778588a1f5c24ba
sas12mita <sumitapoudel12@gmail.com>
2026-05-07 10:21:47 +0800

    quiz

--- randomquiz.py
+++ randomquiz.py
@@ -1,1 +1,31 @@
-#store math
+#store math quiz
+#use random
+#choice[+,-,*,/]
+#shuffle operator
+#ask the question
+#allow the user to give the ans
+#check the ans
+ 
+import random
+operator=['+','-','*','/']
+random.shuffle(operator)
+num1=random.randint(1,10)
+num2=random.randint(1,10)
+op=random.choice(operator)
+user_answer= float(input(f"what is {num1} {op} {num2}: "))
+if op=="+":
+    correct_ans=num1+num2
+elif op=="-":
+    correct_ans=num1-num2
+elif op=="*":
+    correct_ans=num1*num2
+elif op=="/":
+    correct_ans=num1/num2
+else:
+    print("Something went to wrong")
+
+if(user_answer==correct_ans):
+    print("Your answer is correct:",correct_ans)
+else:
+    print("Your answer is wrong and correct answer is: ", correct_ans)
+
