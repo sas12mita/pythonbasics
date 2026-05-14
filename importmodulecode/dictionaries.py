396aa0d2ee0d45e761a090ddf4c699c7be906d55
sas12mita <sumitapoudel12@gmail.com>
2026-05-14 10:07:28 +0800

    dictionary revision

--- importmodulecode/dictionaries.py
+++ importmodulecode/dictionaries.py
@@ -1,6 +1,38 @@
 workshop={
     "Sasmita":
     {
-        "fav_color":"blue"
+        "fav_color":"red",
+        "fav_nul":"WindowsError",
+        "destination":"Pokhara",
+
+    },
+    "Bikash":
+    {
+        "fav_color":"blue",
+        "fav_nul":"WindowsError",
+        "destination":"Pokhara",
+
     }
 }
+print(workshop)
+print(workshop["Sasmita"])
+print(workshop["Bikash"]["destination"])
+
+
+#update dictionary
+workshop["Sasmita"]["fav_nul"]="one",
+print(workshop["Sasmita"])
+
+d={
+
+}
+d[1]=5
+print(d)
+
+#update 
+d.update({
+    2: 10,
+    3: 15
+})
+
+print(d)
